1ecfcc1c628281c2f86e624b9a4b0a054f7fd7bc
Patrick Thompson <patrick@unseenvision.com>
2015-06-28 17:57:36 -0700

    Fix print for Python 2.7 and adjust formatting.

--- dice.py
+++ dice.py
@@ -2,55 +2,64 @@ leave_program = 0
 
 while leave_program != "q":
     import random
-    print("This is a dice rolling program")
-    print("Press enter to roll")
-    input()
-    number = random.randint(1, 6)
-    if number == 1:
-        print ("[--------------------]")
-        print ("[--------------------]")
-        print ("[---------O---------]")
-        print ("[--------------------]")
-        print ("[--------------------]")
-        print("Type 'q' to quit")
-        leave_program = input()
-    if number == 2:
-        print ("[--------------------]")
-        print ("[--------------------]")
-        print ("[-----O-------O----]")
-        print ("[--------------------]")
-        print ("[--------------------]")
-        print("Type 'q' to quit")
-        leave_program = input()
-    if number == 3:
-        print ("[--------------------]")
-        print ("[---------O---------]")
-        print ("[--------------------]")
-        print ("[----O--------O----]")
-        print ("[--------------------]")
-        print("Type 'q' to quit")
-        leave_program = input()
-    if number == 4:
-        print ("[--------------------]")
-        print ("[-----O--------O---]")
-        print ("[---------------------]")
-        print ("[-----O-------O----]")
-        print ("[--------------------]")
-        print("Type 'q' to quit")
-        leave_program = input()
-    if number == 5:
-        print ("[--------------------]")
-        print ("[-----O--------O---]")
-        print ("[----------O--------]")
-        print ("[-----O-------O----]")
-        print ("[--------------------]")
-        print("Type 'q' to quit")
-        leave_program = input()
-    if number == 6:
-        print ("[--------------------]")
-        print ("[---O--------O-----]")
-        print ("[---O--------O-----]")
-        print ("[---O--------O-----]")
-        print ("[--------------------]")
-        print("Type 'q' to quit")
-        leave_program = input()
+
+    print "This is a dice rolling program"
+    print "Press enter to roll"
+
+input()
+
+number = random.randint(1, 6)
+
+if number == 1:
+    print "[--------------------]"
+    print "[--------------------]"
+    print "[---------O---------]"
+    print "[--------------------]"
+    print "[--------------------]"
+    print "Type 'q' to quit"
+    leave_program = input()
+
+if number == 2:
+    print "[--------------------]"
+    print "[--------------------]"
+    print "[-----O-------O----]"
+    print "[--------------------]"
+    print "[--------------------]"
+    print "Type 'q' to quit"
+    leave_program = input()
+
+if number == 3:
+    print "[--------------------]"
+    print "[---------O---------]"
+    print "[--------------------]"
+    print "[----O--------O----]"
+    print "[--------------------]"
+    print "Type 'q' to quit"
+    leave_program = input()
+
+if number == 4:
+    print "[--------------------]"
+    print "[-----O--------O---]"
+    print "[---------------------]"
+    print "[-----O-------O----]"
+    print "[--------------------]"
+    print "Type 'q' to quit"
+    leave_program = input()
+
+if number == 5:
+    print "[--------------------]"
+    print "[-----O--------O---]"
+    print "[----------O--------]"
+    print "[-----O-------O----]"
+    print "[--------------------]"
+    print "Type 'q' to quit"
+    leave_program = input()
+
+if number == 6:
+    print "[--------------------]"
+    print "[---O--------O-----]"
+    print "[---O--------O-----]"
+    print "[---O--------O-----]"
+    print "[--------------------]"
+    print "Type 'q' to quit"
+    leave_program = input()
